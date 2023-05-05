a8c69f9b88db671b9ab97f10f4de2a04869d56c6
kusnkusn <ductiny2003@gmail.com>
2023-05-06 04:53:15 +0700

    mobilestore

--- Template/top-sale.php
+++ Template/top-sale.php
@@ -8,15 +8,16 @@
                 <hr>
                 <!-- owl -->
                 <div class="owl-carousel owl-theme">        
-                    <?php foreach($product_shuffle as $item) {?>           
+                    <?php foreach($product_shuffle as $item) {
+                        if($item["item_last_price"] > 20000000){?>
                     <div class="item py-2">
                         <div class="product font-rale">
-                            <a href="#"><img src="<?php echo $item["item_image"] ?>" alt="product"></a>
+                            <a href="#"><img src="<?php echo $item["item_image"]; ?>" alt="product"></a>
                             <div class="text-center">
-                                <h6>iPhone 14 Pro Max</h6>
+                                <h6><?php echo $item["item_name"]; ?></h6>
                                 <div class="price py-2">
-                                    <span style="color:red" class="px-2">41.790.000 ₫</span>
-                                    <span style="color:gray" class="px-2 font-size-14 text-decoration-line-through">45.990.000 ₫</span> 
+                                    <span style="color:red" class="px-2"><?php echo number_format( $item["item_last_price"], 0, '', '.' );?> ₫</span>
+                                    <span style="color:gray" class="px-2 font-size-14 text-decoration-line-through"><?php echo number_format( $item["item_price"], 0, '', '.' );?> ₫</span> 
                                 </div>
                                 <div class="rating text-warning font-size-12">
                                     <span><i class="fas fa-star"></i></span>
@@ -29,7 +30,7 @@
                             </div>
                         </div>
                     </div>              
-                    <?php } ?>
+                    <?php }} ?>
                 </div>
                 <hr>
             </div>
